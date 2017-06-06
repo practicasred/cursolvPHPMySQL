4ffe3736b4d0c8490e0a64bdc385442cc12db929
practicasred <practicas.red@gmail.com>
2017-06-06 13:19:19 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Curso LV PHP + MySQL</title>
+  </head>
+  <body>
+   <h1>Curso LV PHP + MySQL</h1>
+   <h2><a href="dbtest.php">Prueba la conexión a la base de datos</a></h2>
+  </body>
+</html>
